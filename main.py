d393d5c15df3d8ba72a2981fbd4688a38f2f9ae0
miikawisra <miika.ruutiainen@gmail.com>
2021-12-24 23:12:43 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def toinenpotenssi(eka):
+    eka = int(eka)
+    eka = (eka**2)
+    print("Toinen potenssi on", eka)
+
+
+
+def main():
+    eka = input("Anna lukuarvo:")
+    toinenpotenssi(eka)
+
+
+if __name__ == "__main__":
+    main()
